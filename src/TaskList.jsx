7f342f870f700b03059a249c20d31b89809faefe
Kev Kim <kevin@Kevs-MacBook-Pro.local>
2022-12-10 12:30:45 -0500

    working copy

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,20 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import TaskForm from './TaskForm';
 import TaskItem from './TaskItem';
+import { Form, DropdownButton, Dropdown } from 'react-bootstrap';
 
 const TaskList = () => {
-  // Declare the tasks state variable and the setTasks state updater function
-  // using the useState hook
+  // Declare the tasks, sortOrder, and filter state variables
+  // and the corresponding setter functions using the useState hook
   const [tasks, setTasks] = useState([]);
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [filter, setFilter] = useState('all');
 
   // Define a function to load the tasks from local storage
   const loadTasks = () => {
     // Get the tasks from local storage
     const tasks = JSON.parse(localStorage.getItem('tasks'), ['id', 'title', 'description', 'status']) || [];
   
+    // Apply the selected sort order and filter criteria to the tasks
+    const sortedAndFilteredTasks = applySortAndFilter(tasks);
+
     // Update the tasks state
-    setTasks(tasks);
+    setTasks(sortedAndFilteredTasks);
   };
+
+  // Define a function to apply the selected sort order and filter criteria
+  // to the tasks in the tasks state
+  const applySortAndFilter = tasks => {
+    // Apply the selected sort order
+    if (sortOrder === 'asc') {
+      tasks = tasks.sort((a, b) => a.title.localeCompare(b.title));
+    } else {
+      tasks = tasks.sort((a, b) => b.title.localeCompare(a.title));
+    }
+
+    // Apply the selected filter
+    if (filter !== 'all') {
+      tasks = tasks.filter(task => task.status === filter);
+    }
+
+    return tasks;
+  };  
 
   useEffect(() => {
     // Load the tasks from local storage
@@ -28,7 +52,6 @@ const TaskList = () => {
     return () => window.removeEventListener('storage', loadTasks);
   }, []); // Remove the tasks state variable from the dependency array
   
-
   // Define a new function to handle deleting tasks
   const handleDelete = id => {
     // Get the existing tasks from local storage
@@ -41,23 +64,38 @@ const TaskList = () => {
     // Save the updated tasks array to local storage
     localStorage.setItem('tasks', JSON.stringify(updatedTasks));
 
-    // Log the local storage
-    console.log(localStorage);
+    // Update the tasks state with the updated tasks array
+    setTasks(updatedTasks);
   };
+
 
   return (
     <div className="task-list">
       <h2>Task List</h2>
+  
+      <div className="d-flex justify-content-between mb-3">
+        <DropdownButton title="Sort Order" className="mr-3">
+          <Dropdown.Item onClick={() => setSortOrder('asc')}>Ascending</Dropdown.Item>
+          <Dropdown.Item onClick={() => setSortOrder('desc')}>Descending</Dropdown.Item>
+        </DropdownButton>
+        <DropdownButton title="Filter">
+          <Dropdown.Item onClick={() => setFilter('all')}>All</Dropdown.Item>
+          <Dropdown.Item onClick={() => setFilter('pending')}>Pending</Dropdown.Item>
+          <Dropdown.Item onClick={() => setFilter('completed')}>Completed</Dropdown.Item>
+        </DropdownButton>
+      </div>
+  
       <ul>
-        {tasks.map(task => (
+        {applySortAndFilter(tasks).map(task => (
           // For each task in the tasks array, render a TaskItem component
           <TaskItem key={task.id} task={task} handleDelete={handleDelete} />
         ))}
       </ul>
+  
       {/* Pass the loadTasks function to the TaskForm component as a prop */}
       <TaskForm loadTasks={loadTasks} />
     </div>
-  );
+  );  
 };
 
 export default TaskList;
