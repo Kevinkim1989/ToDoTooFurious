395a1d350df342e1af0b978ad74e46ad9153f748
Kev Kim <kevin@Kevs-MacBook-Pro.local>
2022-12-10 09:43:25 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import TaskList from './TaskList';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import $ from 'jquery';
-// import 'popper.js';
-// import 'bootstrap';
-// import { Button } from 'react-bootstrap';
 
 export default () => (
   <>
